1651f5a98dcaa71cd0661adbf0feacc498679b1e
Cat Plus Plus <piotrlegnica@piotrl.pl>
2013-08-10 22:41:08 +0200

    A very large jumbotron.

---
+++ make.py
@@ -0,0 +1,39 @@
+#!python3
+import os, sys, collections
+import jinja2
+
+sections = collections.OrderedDict()
+
+base_context = {
+    'PROJECT':  'Lounge<Chat>',
+    'HTTP':     'http:' if len(sys.argv) < 2 or not sys.argv[1].startswith('prod') else '',
+    'SECTIONS': sections,
+}
+
+loader = jinja2.FileSystemLoader('src')
+env    = jinja2.Environment(loader = loader, autoescape = True)
+
+def add_section(name):
+    sections[name] = { 'name': name, 'title': name.title() }
+
+def render_part(name, is_section = True):
+    html_name = name + '.html'
+    template  = env.get_template(html_name)
+    context   = dict(base_context)
+
+    if is_section:
+        context['current_section'] = sections[name]
+
+    result = template.render(**context)
+
+    with open(html_name, 'w', encoding = 'utf-8') as fp:
+        fp.write(result)
+
+def render():
+    render_part('index', is_section = False)
+    for section in sections:
+        render_part(section)
+
+add_section('organisation')
+add_section('contributing')
+render()
