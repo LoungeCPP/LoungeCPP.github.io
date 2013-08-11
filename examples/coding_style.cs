39293363a2bbe4b0f381379ac7150a4eae9b07db
Cat Plus Plus <piotrlegnica@piotrl.pl>
2013-08-11 17:51:26 +0200

    Fixes for the generator, and more content.

---
+++ examples/coding_style.cs
@@ -0,0 +1,97 @@
+#region Apache License 2.0
+
+// Copyright 2013 Lounge<Chat> Team
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. 
+
+#endregion
+
+namespace LoungeChat.Server.Components {
+    using System;
+    using System.Collections.Generic;
+
+    using ThirdPartyLibrary;
+
+    using Services;
+
+    public sealed class Component : IService1, IService2 {
+        private readonly ILog _logger;
+        private int _test;
+
+        private const string Something = "...";
+
+        public int Test {
+            get { return _test; }
+            set { _test = value; }
+        }
+
+        public int Test2 { get; set; }
+
+        public Server(ILog logger) {
+            _logger = logger;
+        }
+
+        private void Test() {
+            throw new Exception("Abababa");
+        }
+
+        private void Test2() {
+            try {
+                Test();
+            } catch (Exception e) {
+                throw new Exception("blblblb", e);
+            }
+        }
+
+        #region Implementation of IService1
+
+        public void DoThing1() {
+            _logger.Debug("Debug");
+            _logger.Info("Start()");
+            _logger.Warning("Warning");
+            _logger.Error("Error");
+            _logger.Fatal("Fatal");
+
+            try {
+                Test();
+            } catch (Exception e) {
+                _logger.Error(e, "Exception1");
+            }
+
+            try {
+                Test2();
+            } catch (Exception e) {
+                _logger.Error(e, "Exception2");
+            }
+
+            foreach (var x in xs) {
+            }
+
+            for (var idx = 0; idx < xs.Length; ++idx) {
+            }
+
+            var soChatSucks = true;
+            while (soChatSucks) {
+            }
+        }
+
+        #endregion
+
+        #region Implementation of IService2
+
+        public void DoThing2() {
+        }
+
+        #endregion
+    }
+}
--- make.py
+++ make.py
@@ -1,17 +1,16 @@
 #!python3
 import os, sys, collections
-import jinja2
+import jinja2, pygments, pygments.lexers, pygments.formatters
 
-sections = collections.OrderedDict()
+def highlight(example):
+    with open('examples/' + example, 'r', encoding = 'utf-8') as fp:
+        code = fp.read()
 
-base_context = {
-    'PROJECT':  'Lounge<Chat>',
-    'HTTP':     'http:' if len(sys.argv) < 2 or not sys.argv[1].startswith('prod') else '',
-    'SECTIONS': sections,
-}
+    lexer       = pygments.lexers.get_lexer_for_filename(example)
+    formatter   = pygments.formatters.HtmlFormatter()
+    highlighted = pygments.highlight(code, lexer, formatter)
 
-loader = jinja2.FileSystemLoader('src')
-env    = jinja2.Environment(loader = loader, autoescape = True)
+    return jinja2.Markup(highlighted)
 
 def add_section(name):
     sections[name] = { 'name': name, 'title': name.title() }
@@ -26,6 +25,10 @@ def render_part(name, is_section = True):
 
     result = template.render(**context)
 
+    if not production:
+        os.makedirs('tmp', exist_ok = True)
+        html_name = 'tmp/' + html_name
+
     with open(html_name, 'w', encoding = 'utf-8') as fp:
         fp.write(result)
 
@@ -33,6 +36,19 @@ def render():
     render_part('index', is_section = False)
     for section in sections:
         render_part(section)
+
+sections     = collections.OrderedDict()
+production   = len(sys.argv) >= 2 and sys.argv[1].startswith('prod')
+base_context = {
+    'PROJECT':   'Lounge<Chat>',
+    'HTTP':      '' if production else 'http:',
+    'BASE':      '' if production else '../',
+    'SECTIONS':  sections,
+    'highlight': highlight,
+}
+
+loader = jinja2.FileSystemLoader('src')
+env    = jinja2.Environment(loader = loader, autoescape = True)
 
 add_section('project')
 add_section('contributing')
